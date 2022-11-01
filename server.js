ada7969ca2d4cb6b964a19512417581f155bf20d
chris1797 <jaehun6004@gmail.com>
2022-11-01 22:38:39 +0900

    2022-11-01

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /* http 사용
 // http를 쓰면 if문으로 url별 로직을 구현해줘야 되는데 때문에 express를 쓰는 것
 const http = require("http");
@@ -14,35 +16,24 @@ app.listen(3001, () => {
   console.log("http server on");
 });
 */
-
 //--------------------------------------------------------------------
-
-// express 사용법, 기본으로 해줘야 함
+// 모듈
 const express = require('express');
 const app = express();
 
-/** 
- * 서버 재시동 자동화
- * 터미널에서 nodemon server.js
- */
+// 로컬 포트
+const PORT = 8080;
+
+// 라우팅
+const home = require("./routes/home");
 
 // 앱 세팅
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
+app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드.
+
 // .listen()을 치면 로컬에서 서버를 열 수 있음
-app.listen(8080, () => {
+app.listen(PORT, () => {
   console.log('listening on 8080')
-});
-
-/**
- * 서버로 GET요청 처리
- * app.get('경로', function(요청, 응답){});
- */ 
-app.get('/home', function(req, res){
-  res.send('home 입니다.')
-});
-
-app.get('/login', function(req, res){
-  res.sendFile(__dirname + '/index.html')
 });
