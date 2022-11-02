afa9f7c80205459f76e1e6144433cf85a6955881
chris1797 <jaehun6004@gmail.com>
2022-11-02 16:02:56 +0900

    login fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -12,5 +12,14 @@ function login() {
     id: id.value,
     pw: pw.value,
   };
-  console.log(req);
+  // 어떤 경로로 데이터를 주고받을지 정해줘야 함
+  fetch("/login", {
+  // body라는 key값으로 req 데이터를 JSON형태로 감싸서 전달
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"  // 내가 보내는 데이터 타입은 JSON이다.
+    }
+    body: JSON.stringify(req),
+
+  });
 }
