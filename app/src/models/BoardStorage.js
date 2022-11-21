4cb7bdb184e9426745249135fd24c16409952f5c
chris1797 <jaehun6004@gmail.com>
2022-11-21 22:30:45 +0900

    BoardStorage 추가

---
+++ app/src/models/BoardStorage.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const db = require("../config/db");
+
+class BoardStorage {
+  /**
+   * 글 상세보기
+   * @param {*} id
+   * @returns
+   */
+  static getBoardDetail(id) {
+    return new Promise((resolve, reject) => {
+      const query = "SELECT * FROM board WHERE id = ?;";
+      db.query(query, [id], (err, data) => {
+        if (err) reject(`${err}`);
+        resolve(data[0]);
+      });
+    });
+  }
+
+  static async save(board) {
+    return new Promise((resolve, reject) => {
+      const query =
+        "INSERT INTO board(id, name, title, content) VALUES(?, ?, ?, ?);";
+      db.query(
+        query,
+        [board.id, board.name, board.title, board.content],
+        (err) => {
+          if (err) reject(`${err}`);
+          resolve({ success: true });
+        }
+      );
+    });
+  }
+}
